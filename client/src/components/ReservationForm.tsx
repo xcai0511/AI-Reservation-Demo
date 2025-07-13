2c463aa3cacf5d6fb269ff3c19838454be5074d5
Xiaojin Cai <xcai0511@gmail.com>
2025-07-12 23:36:14 -0400

    Update ReservationForm.tsx

--- client/src/components/ReservationForm.tsx
+++ client/src/components/ReservationForm.tsx
@@ -40,9 +40,9 @@ const ReservationForm = ({ onSubmit, onBack }: ReservationFormProps) => {
   });
 
   const timeSlots = [
-    "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", 
-    "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", 
-    "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM"
+    "5:00 PM", "6:00 PM",
+    "7:00 PM", "8:00 PM",
+    "9:00 PM", "10:00 PM",
   ];
 
   const navigate = useNavigate();
